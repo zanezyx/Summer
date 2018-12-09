834e2e2a38bf1ec50eb40cb0881e97cf673e1b3a
zanezyx <79255569@qq.com>
2018-12-09 22:49:51 +0800

    add login, register activity

---
+++ TaxManager/app/src/main/java/com/deter/TaxManager/RetrivePasswordActivity.java
@@ -0,0 +1,35 @@
+package com.deter.TaxManager;
+
+
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+
+public class RetrivePasswordActivity extends Activity {
+
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);// 无标题栏
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);// 不能横屏
+		setContentView(R.layout.retrive_password_activity);
+//			init();
+
+	}
+	
+	
+	public void toBack(View v)
+	{
+		finish();
+	}
+	
+	
+	public void toGetValideCode(View v)
+	{
+		
+	}
+	
+}
