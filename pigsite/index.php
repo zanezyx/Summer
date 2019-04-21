c58002bcdea3c48b15363a296169803388f99ac5
zanezyx <79255569@qq.com>
2019-04-21 22:10:39 +0800

    add pig site files

---
+++ pigsite/index.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+<meta name="viewport" content="width=device-width,maximum-scale=1.0, minimum-scale=1.0, user-scalable=no">  
+<head>
+</head>
+
+<body>
+<img src="WechatIMG8.jpeg" style='width:100%' alt=""/>
+</body>
+</html>
